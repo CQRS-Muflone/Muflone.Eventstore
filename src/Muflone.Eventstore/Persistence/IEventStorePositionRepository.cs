98b467fb6a23b9270beeb391a28aceaed6c681b6
Alessandro Colla <alessandro.colla@outlook.com>
2018-10-26 22:16:28 +0200

    Add first version

---
+++ src/Muflone.Eventstore/Persistence/IEventStorePositionRepository.cs
@@ -0,0 +1,11 @@
+﻿using System.Threading.Tasks;
+using EventStore.ClientAPI;
+
+namespace Muflone.Eventstore.Persistence
+{
+  public interface IEventStorePositionRepository
+  {
+    Task<Position> GetLastPosition();
+    Task Save(long commitPosition, long preparePosition);
+  }
+}
